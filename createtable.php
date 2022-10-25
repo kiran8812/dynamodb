417b0f7c04b456abbec9e5e5a338c7f2dd06ef17
kiran8812 <116144797+kiran8812@users.noreply.github.com>
2022-10-25 11:46:21 +0530

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,1 +1,29 @@
-salksjaskl
+        $tableName = "ddb_demo_table_$uuid";
+        $service->createTable(
+            $tableName,
+            [
+                new DynamoDBAttribute('year', 'N', 'HASH'),
+                new DynamoDBAttribute('title', 'S', 'RANGE')
+            ]
+        );
+
+    public function createTable(string $tableName, array $attributes)
+    {
+        $keySchema = [];
+        $attributeDefinitions = [];
+        foreach ($attributes as $attribute) {
+            if (is_a($attribute, DynamoDBAttribute::class)) {
+                $keySchema[] = ['AttributeName' => $attribute->AttributeName, 'KeyType' => $attribute->KeyType];
+                $attributeDefinitions[] =
+                    ['AttributeName' => $attribute->AttributeName, 'AttributeType' => $attribute->AttributeType];
+            }
+        }
+
+        $this->dynamoDbClient->createTable([
+            'TableName' => $tableName,
+            'KeySchema' => $keySchema,
+            'AttributeDefinitions' => $attributeDefinitions,
+            'ProvisionedThroughput' => ['ReadCapacityUnits' => 10, 'WriteCapacityUnits' => 10],
+        ]);
+    }
+
